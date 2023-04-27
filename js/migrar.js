2530d921a6a44d29015a56c8e7740cf5a8d8a608
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2023-04-26 23:47:27 -0300

    Add files via upload

---
+++ js/migrar.js
@@ -0,0 +1,46 @@
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+if (localStorage.getItem("token") == null) {
+  alert("Você precisa estar logado para acessar essa página");
+  window.location.href = "./index.html";
+}
+
+function handleClean() {
+  const number = document.querySelector("#number").value;
+
+  const endpoint = `https://wom.gvt.com.br:19900/wfm/mobile/migration/migrationPlatformTV.faces?backUrl=https%3A%2F%2Fgvt.etadirect.com%2Fmobility%2F&aid=${number}`;
+
+  const iframe = document.querySelector("#certification-frame");
+  if (iframe) {
+    iframe.remove();
+  }
+
+  const newIframe = document.createElement("iframe");
+  newIframe.id = "certification-frame";
+  newIframe.src = endpoint;
+  document.body.appendChild(newIframe);
+}
+
+function handleLogout() {
+  document.body.classList.add("blur");
+  localStorage.removeItem("token");
+  setTimeout(() => {
+    window.location.href = "./index.html";
+  }, 500);
+}
