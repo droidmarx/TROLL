248963f64d4832f041c005f3b6334a1644b008d7
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2023-05-02 12:40:30 -0300

    Atualizar o index.js

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ const users = [
   { username: "Simone", password: "102030" },
   { username: "Leandro", password: "102030" },
   { username: "Dianini", password: "102030" },
+{ username: "Joel", password: "102030" },
   // adicionar mais usuarios
 ];
 
