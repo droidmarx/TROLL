c4c9a527509bdb5c80871cb841faed4b9ab1d4ad
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2023-07-18 07:31:57 -0300

    Add files via upload

--- js/buscar.js
+++ js/buscar.js
@@ -16,30 +16,82 @@ function toggleMenu(event) {
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
 
-if (localStorage.getItem("token") == null) {
-  alert("Você precisa estar logado para acessar essa página");
-  window.location.href = "./index.html";
+// Obtém os itens do menu 
+const menuItems = document.querySelectorAll("#menu li a");
+
+// Função para remover a classe "active" do elemento de navegação 
+function removeActiveClass() {
+  const nav = document.getElementById("nav");
+  nav.classList.remove("active");
 }
 
+// Adiciona o evento de clique a cada item do menu 
+menuItems.forEach(function(item) {
+  item.addEventListener("click", removeActiveClass);
+});
+
+// Mostrar Limpar
+function mostrarLimpar() {
+  document.getElementById('limpar').style.display = 'block';
+  document.getElementById('migrar').style.display = 'none';
+  
+}
+
+// Mostrar Migrar
+function mostrarMigrar() {
+  document.getElementById('migrar').style.display = 'block';
+  document.getElementById('limpar').style.display = 'none';
+}
+
+
+// Limpar Loading
 function handleClean() {
+  const loadingContainer = document.querySelector('.loading-container');
+  loadingContainer.style.display = 'flex';
   const number = document.querySelector("#number").value;
-
   const endpoint = `https://wom.gvt.com.br:19900/wfm/certificationClean/certificationClean.faces?&aid=${number}&userlogin=x&atoken=gvt`;
-
   const iframe = document.querySelector("#certification-frame");
   if (iframe) {
     iframe.remove();
   }
-
   const newIframe = document.createElement("iframe");
   newIframe.id = "certification-frame";
   newIframe.src = endpoint;
+  newIframe.addEventListener('load', () => {
+    loadingContainer.style.display = 'none';
+  });
   document.body.appendChild(newIframe);
 }
 
+// Migrar Loading
+function handleMigrate() {
+  const loadingContainer = document.querySelector('.loading-container');
+  loadingContainer.style.display = 'flex';
+  const number = document.querySelector("#number").value;
+  const endpoint = `https://wom.gvt.com.br:19900/wfm/mobile/migration/migrationPlatformTV.faces?backUrl=https%3A%2F%2Fgvt.etadirect.com%2Fmobility%2F&aid=${number}`;
+  const iframe = document.querySelector("#certification-frame");
+  if (iframe) {
+    iframe.remove();
+  }
+  const newIframe = document.createElement("iframe");
+  newIframe.id = "certification-frame";
+  newIframe.src = endpoint;
+  newIframe.addEventListener('load', () => {
+    loadingContainer.style.display = 'none';
+  });
+  document.body.appendChild(newIframe);
+}
+
+// Logout
 function handleLogout() {
   localStorage.removeItem("token");
   setTimeout(() => {
     window.location.href = "./index.html";
   }, 200);
+}
+
+// Verificar token e redirecionar para a página de login se não estiver logado
+if (localStorage.getItem("token") == null) {
+  alert("Você precisa estar logado para acessar essa página");
+  window.location.href = "./index.html";
 }
--- js/index.js
+++ js/index.js
@@ -2,22 +2,22 @@ const form = document.querySelector("form");
 const loginBtn = document.querySelector("#login-btn");
 
 const users = [
-  { username: "Gui", password: "Marx" },
-  { username: "Rivaldo", password: "102030" },
-{ username: "Emilson", password: "Emilson123" },
-  { username: "Bigshark", password: "Bigdick" },
-  { username: "Felipe", password: "Cheira" },
-  { username: "Gustavo", password: "Alves" },
-  { username: "Douglas", password: "Rosa" },
-  { username: "Lucas", password: "Conti" },
-  { username: "Rafael", password: "Borges" },
-  { username: "Adriano", password: "270301" },
-  { username: "Simone", password: "102030" },
-  { username: "Leandro", password: "102030" },
-  { username: "Wendel", password: "Drumil" },
-{ username: "Dayvison", password: "102030" },
-  // adicionar mais usuarios
-];
+ { username: "Gui", password: "Marx" },
+ { username: "Rivaldo", password: "102030" },
+ { username: "Emilson", password: "Emilson123" },
+ { username: "Bigshark", password: "Bigdick" },
+ { username: "Felipe", password: "Cheira" },
+ { username: "Gustavo", password: "Alves" },
+ { username: "Douglas", password: "Rosa" },
+ { username: "Lucas", password: "Conti" },
+ { username: "Rafael", password: "Borges" },
+ { username: "Adriano", password: "270301" },
+ { username: "Simone", password: "102030" },
+ { username: "Leandro", password: "102030" },
+ { username: "Wendel", password: "Drumil" },
+ { username: "Dayvison", password: "102030" },
+   // adicionar mais usuarios 
+ ];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
