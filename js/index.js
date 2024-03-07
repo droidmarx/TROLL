cf34e04eb56fa2982415aa2c4493f3f8aea8988d
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2024-03-07 19:50:36 -0300

    Atualizar o index.js

--- js/index.js
+++ js/index.js
@@ -3,20 +3,7 @@ const loginBtn = document.querySelector("#login-btn");
 
 const users = [
  { username: "Gui", password: "Marx" },
- { username: "Rivaldo", password: "102030" },
- { username: "Emilson", password: "Emilson123" },
- { username: "Bigshark", password: "Bigdick" },
- { username: "Felipe", password: "Cheira" },
- { username: "Gustavo", password: "Alves" },
- { username: "Douglas", password: "Rosa" },
  { username: "Lucas", password: "Conti" },
- { username: "Rafael", password: "Borges" },
- { username: "Adriano", password: "270301" },
- { username: "Simone", password: "102030" },
- { username: "Leandro", password: "102030" },
- { username: "Wendel", password: "Drumil" },
- { username: "Dayvison", password: "102030" },
-{ username: "Anderson", password: "Dton" },
    // adicionar mais usuarios 
  ];
 
